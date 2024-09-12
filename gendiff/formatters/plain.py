1b4189fad1bb34d63a169b21a1015a1d53a9bd7e
Jane Drobova <janejuly1@gmail.com>
2024-09-12 17:12:11 +0300

    fix line length

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -20,7 +20,8 @@ def format_plain(diff):
             elif change['status'] == 'changed':
                 old_value = change['old_value']
                 new_value = change['new_value']
-                result.append(f"Property '{new_path}' was updated. From {format_value(old_value)} to {format_value(new_value)}")
+                result.append(f"Property '{new_path}' was updated. "
+                              f"From {format_value(old_value)} to {format_value(new_value)}")
             elif change['status'] == 'nested':
                 recurse(new_path, change['value'])
 
